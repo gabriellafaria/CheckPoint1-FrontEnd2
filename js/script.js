7d2e7a250ee03e30dd82b4a8cdd01244a434f450
GabriellaFaria <gabisouf@gmail.com>
2022-06-14 20:20:20 -0300

    ajustes finais

--- js/script.js
+++ js/script.js
@@ -2,37 +2,57 @@
 let fatherElement = document.getElementById('fatherElement');
 var title = document.getElementById('iTitle');
 var image = document.getElementById('iCover');
-var description = document.getElementById('iSynopsis');
+var synopsis = document.getElementById('iSynopsis');
+var author = document.getElementById('iAuthor');
 
 // função de callback para atualização dinâmica
 function generator(calFunction){
     showCards(calFunction);
+    clean()
 }
 
 function showCards() {
     // são os elementos que recebemos do formulario
-    let titleS = title.value;
-    let imageS = image.value;
-    let descriptionS = description.value;
     
     let newCard = document.createElement('div');
     newCard.classList.add('item');
     newCard.innerHTML = `
-        <h1>${titleS}</h1>
-        <img src="${imageS}">
-        <p>${descriptionS}</p>
+        <h1>${title.value}</h1>
+        <img src="${image.value}">
+        <h3>${author.value}</h3>
+        <p>${synopsis.value}</p>
     `
     fatherElement.appendChild(newCard);
 }; 
 
-// botão de envio
 let sendButton = document.getElementById('send');
+let nameValidation = document.getElementById('nameValidation');
+let synopsisValidation = document.getElementById('synopsisValidation');
+// eventos do botão de envio
 sendButton.addEventListener('click', (answer) => {
-    if(title.value && description.value) {
     answer.preventDefault();
-    generator();
+    if (title.value && synopsis.value) {
+        generator();
+        nameValidation.innerText = null;
+        synopsisValidation.innerText = null;
+    } else if (title.value) {
+        synopsisValidation.innerText = "Preencha a sinopse do livro";
+        nameValidation.innerText = null;
+    } else if (synopsis.value) {
+        nameValidation.innerText = "Preencha o nome do livro";
+        synopsisValidation.innerText = null
     } else {
-        answer.preventDefault();
-        alert('Insira o título e a sinopse do livro.')
+        nameValidation.innerText = "Preencha o nome do livro"; 
+        synopsisValidation.innerText = "Preencha a sinopse do livro";
     }
 }); 
+
+function clean() {
+    let information = document.querySelectorAll('.info');
+    for (let i of information) {
+        i.value = null;
+    }
+    nameValidation.innerText = null;
+    synopsisValidation.innerText = null;
+}
+
